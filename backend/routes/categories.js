1ffb1b6139a3ac2cf0b8e4af6ce893afe283487b
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-03-21 01:05:35 +0300

    added morgan

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/category")
+
+
+// Yeni bir kategori oluşturma (Create)
+router.post("/", async (req, res) => {
+    try {
+
+        const { name, img } = req.body;
+
+        const newCategory = new Category({name, img});
+
+        await newCategory.save();
+
+        res.status(201).json(newCategory);
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 
 // Tüm kategorileri getirme (Read-All)
 router.get("/", async (req, res) => {
-    res.send("Kategoriler getirildi")
+    try {
+        const categories = await Category.find()
+        res.status(200).json(categories)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: "Server error."})
+    }
 })
 
 module.exports = router;
