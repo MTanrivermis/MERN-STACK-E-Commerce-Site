dd9570785e0d2d02d9eb36570948fcdde2cd50b8
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-04-21 23:40:44 +0300

    bcrypt auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 // Diğer routes dosylarını içe aktarıyoruz.
 const categoryRoute = require("./categories.js");
+const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
 
 
 // Her route ilgili yol altında kullanıyoruz.
 router.use("/categories", categoryRoute)
+router.use("/auth", authRoute)
 router.use("/products", productRoute)
 
 module.exports = router;
