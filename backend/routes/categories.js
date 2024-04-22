1901a49aa546e9a497dbb6d39e2bc11ddfe7a23f
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-04-22 14:40:57 +0300

    Created product model

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../models/category");
+const Category = require("../models/Category");
 
 // Yeni bir kategori oluşturma (Create)
 router.post("/", async (req, res) => {
