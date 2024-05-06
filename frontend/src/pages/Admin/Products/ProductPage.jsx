5ce0d3b26ac81058d212b9e3d240aa67ae2c2c57
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-05-06 23:33:25 +0300

    up

---
+++ frontend/src/pages/Admin/Products/ProductPage.jsx
@@ -0,0 +1,148 @@
+import { Button, Popconfirm, Space, Table, message } from "antd";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ProductPage = () => {
+  const [dataSource, setDataSource] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate()
+
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const columns = [
+    {
+      title: "Product Image",
+      dataIndex: "img",
+      key: "img",
+      render: (imgSrc) => <img src={imgSrc[0]} alt="Image" width={100} />,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <b>{text}</b>,
+    },
+    {
+      title: "Category",
+      dataIndex: "categoryName",
+      key: "categoryName",
+      render: (text) => <span>{text}</span>,
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key:"price",
+      render: (text) => <span>{text.current.toFixed(2)}</span>,
+    },
+    {
+      title: "Discount",
+      dataIndex: "price",
+      key:"price",
+      render: (text) => <span>%{text.discount}</span>,
+    },
+    {
+      title: "Creation Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_, record) => (
+        <Space>
+            <Button type="primary" onClick={()=> navigate(`/admin/products/update/${record._id}`)}>
+              Update
+            </Button>
+          <Popconfirm
+            title="Delete the category"
+            description="Are you sure to delete this category?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={() => deleteProduct(record._id)}
+          >
+            <Button type="primary" danger>
+              Delete
+            </Button>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+
+
+  const deleteProduct = async (productId) => {
+    try {
+      const response = await fetch(`${apiUrl}/api/products/${productId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        message.success("Category deleted successfully");
+        // fetchCategories();
+        setDataSource((prevProducts) => {
+          return prevProducts.filter((product) => product._id !== productId)
+        })
+      } else {
+        message.error("Deletion failed");
+      }
+    } catch (error) {
+      console.log("Delete Error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        
+        const [categoriesResponse, productResponse] = await Promise.all([
+          fetch(`${apiUrl}/api/categories`),
+          fetch(`${apiUrl}/api/products`),
+        ]);
+
+        if(!categoriesResponse.ok || !productResponse.ok){
+          message.error("Data fetch failed")
+        }
+  
+        const [categoriesData, productsData] = await Promise.all([
+          categoriesResponse.json(),
+          productResponse.json(),
+        ]);
+
+        const productsWithCategories = productsData.map((product) => {
+          const categoryId = product.category;
+          const category = categoriesData.find(
+            (item) => item._id === categoryId
+          );
+
+          return {
+            ...product,
+            categoryName: category ? category.name : "",
+          }
+        });
+        setDataSource(productsWithCategories)
+      } catch (error) {
+        console.log("Data Error", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData()
+  }, [apiUrl]);
+
+  return (
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+      rowKey={(record) => record._id}
+      loading={loading}
+    />
+  );
+};
+
+export default ProductPage;
