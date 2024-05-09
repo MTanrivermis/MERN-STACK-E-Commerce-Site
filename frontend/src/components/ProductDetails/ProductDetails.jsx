8ef69b15655990b3345f86931dd9b1f6386c08d6
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-05-10 01:04:42 +0300

    up

--- frontend/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/components/ProductDetails/ProductDetails.jsx
@@ -1,10 +1,11 @@
 import Breadcrumb from "./Breadcrumb/Breadcrumb"
 import Gallery from "./Gallery/Gallery"
 import Info from "./Info/Info"
-import "./ProductDetails.css"
 import Tabs from "./Tabs/Tabs"
+import PropTypes from "prop-types"
+import "./ProductDetails.css"
 
-const ProductDetails = () => {
+const ProductDetails = ({singleProduct}) => {
     return (
         <section className="single-product">
             <div className="container">
@@ -12,7 +13,7 @@ const ProductDetails = () => {
                     <Breadcrumb />
                     <div className="single-content">
                         <main className="site-main">
-                            <Gallery />
+                            <Gallery singleProduct={singleProduct} />
                             <Info />
                         </main>
                     </div>
@@ -27,3 +28,7 @@ const ProductDetails = () => {
 }
 
 export default ProductDetails
+
+ProductDetails.propTypes = {
+    singleProduct: PropTypes.object,
+};
--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,35 @@
-import ProductDetails from "../components/ProductDetails/ProductDetails"
-
+import { useEffect, useState } from "react";
+import ProductDetails from "../components/ProductDetails/ProductDetails";
+import { useParams } from "react-router-dom";
 
 const ProductDetailsPage = () => {
-    return (
-        <ProductDetails />
-    )
-}
+  const [singleProduct, setSingleProduct] = useState(null);
+  const { id: productId } = useParams();
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
-export default ProductDetailsPage
+  useEffect(() => {
+    const fetchSingleProduct = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/products/${productId}`);
+
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+
+        const data = await response.json();
+        setSingleProduct(data);
+      } catch (error) {
+        console.log("Data Error", error);
+      }
+    };
+    fetchSingleProduct();
+  }, [apiUrl, productId]);
+
+  return singleProduct ? (
+    <ProductDetails singleProduct={singleProduct} setSingleProduct={setSingleProduct} />
+  ) : (
+    <p>Product uploading..</p>
+  );
+};
+
+export default ProductDetailsPage;
