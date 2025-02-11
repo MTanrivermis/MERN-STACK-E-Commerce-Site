33751a33c87835ec82c875ba9ebbfd8735f415f7
MTanrivermis <tanrivermis.mehmet@gmail.com>
2025-02-11 23:20:20 +0300

    up

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,25 +1,24 @@
-import Blogs from "../components/Blogs/Blogs"
-import Brands from "../components/Brands/Brands"
-import CampaignSingle from "../components/CampaignSingle/CampaignSingle"
-import Campaigns from "../components/Campaigns/Campaigns"
-import Products from "../components/Products/Products"
-import Slider from "../components/Slider/Slider"
-import Categories from "../components/categories/categories"
-
+import Blogs from "../components/Blogs/Blogs";
+import Brands from "../components/Brands/Brands";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import Campaigns from "../components/Campaigns/Campaigns";
+import Products from "../components/Products/Products";
+import Slider from "../components/Slider/Slider";
+import Categories from "../components/Categories/Categories";
 
 const HomePage = () => {
-    return (
-        <>
-            <Slider />
-            <Categories />
-            <Products />
-            <Campaigns />
-            <Products />
-            <Blogs />
-            <Brands />
-            <CampaignSingle />
-        </>
-    )
-}
+  return (
+    <>
+      <Slider />
+      <Categories />
+      <Products />
+      <Campaigns />
+      <Products />
+      <Blogs />
+      <Brands />
+      <CampaignSingle />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
